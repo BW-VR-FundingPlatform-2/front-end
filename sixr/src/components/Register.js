5f7c1758042ad961ec8433c44099584317b45628
Ryan Schueck <ryschueck@gmail.com>
2020-05-22 19:20:48 -0400

    added axios to a placeholder api

--- sixr/src/components/Register.js
+++ sixr/src/components/Register.js
@@ -3,11 +3,8 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -45,12 +42,21 @@ export default function Register() {
         password: ''
     })
 
+    const [users, setUsers] = useState([])
+
     const changeHandler = event => {
         setFormState({ ...formState, [event.target.name]: event.target.value })
     }
 
     const Submit = event => {
         event.preventDefault()
+
+            axios.post('https://reqres.in/api/users', formState)
+            .then(res =>{
+                setUsers([...users, res.data])
+                console.log(res.data)
+            })
+
 
         setFormState({
             id: '',
